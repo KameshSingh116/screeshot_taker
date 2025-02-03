7792f1ec07e7eb37f2dd09a968a6dd7deed48d16
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-02-03 18:55:17 +0530

    finishing

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ image=ps.grab(bbox=(10, 10, 500, 500))
 
 image.show()
 
-image.save("1.py","png")
+# image.save(r"C:\Users\lenovo\Desktop\1.png", "png") 
+#"r" is for raw string, it is used to ignore the escape sequences in the string.
+image.save("C:\\Users\\lenovo\\Desktop\\2.png", "png")
 
