2e806070c1e355c91d7a9d0d9e03aa0a73fd7265
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-02-03 19:04:51 +0530

    ending untill further changes

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ image=ps.grab(bbox=(10, 10, 500, 500))
 
 image.show()
 
+path=input("Enter the Path to save the image:")
+
+filename=input("Enter the filename:")
+
+extension=input("Enter the extension of the file:")
 # image.save(r"C:\Users\lenovo\Desktop\1.png", "png") 
 #"r" is for raw string, it is used to ignore the escape sequences in the string.
-image.save("C:\\Users\\lenovo\\Desktop\\2.png", "png")
+image.save(f"{path}/{filename}.{extension}", extension)
+
 
